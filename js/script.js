2769ad113fd6bfe8db18e113cfc670035fad7fea
rodrigocoe1 <rodrigocoe2@gmail.com>
2023-01-19 15:32:17 -0300

    terminando o TCC

--- js/script.js
+++ js/script.js
@@ -1,35 +1,32 @@
-const mario =document.querySelector('.mario')
-const pipe =document.querySelector('.pipe')
-
-const jump =() => {
-    mario.classList.add('jump')
-
-    setTimeout(() => {
-        mario.classList.remove('jump')
-    }, 500)
-}
-
-const loop = setInterval(() => {
-
-    const pipePosition = pipe.offsetLeft
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
-
-    if (pipePosition <= 120 && marioPosition < 80){
-
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`
-        
-        mario.style.animation = "none";
-        mario.style.left = `${marioPosition}px`
-
-        mario.src = './images/game-over.png'
-        mario.style.width = '75px'
-        mario.style.marginLeft ='50px'
-
-        clearInterval(loop)
-    }
+window.onload = function(){
+    const mario = document.querySelector('.game-board .mario');
+    const pipe = document.querySelector('.game-board .pipe');
     
-}, 10);
-
-
-document.addEventListener('keydown', jump )
+    const jump=()=>{
+      mario.classList.add('jump');
+      setTimeout(()=>{
+        mario.classList.remove('jump');
+      },500)
+    }
+  
+    const loop =setInterval(()=>{
+        // posição do cano
+      const pipePosition = pipe.offsetLeft;
+      //quando omario bater no cano ele não descer pro chão e volta a anda
+      const marioPosition = +window.getComputedStyle(mario).bottom.replace('px',''); 
+      if(pipePosition <=120 && pipePosition >0 && marioPosition < 80){
+  
+        pipe.style.animation= 'none';
+        pipe.style.left = `${pipePosition}px`;
+      
+        mario.style.animation= 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = '../images/game-over.png'
+        mario.style.width= '250px';
+        mario.style.marginLeft='50px';
+      }
+    },10)
+    
+    
+    document.addEventListener('keydown', jump); //faaze com que o botao faca o mario pula
+    }
